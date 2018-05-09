ab925c0716cdc0576dd9a9b2e3a820b4d0d0ad8b
Joe Bonniwell <joe@joebonniwell.com>
2018-05-09 09:03:32 -0500

    Add headers to exercise 1-3

--- src/exercise1-3.c
+++ src/exercise1-3.c
@@ -12,10 +12,11 @@ int main() {
 	upper = 300;
 	step = 20;
 
+	printf("Fahrenheit Celsius\n");
 	fahr = lower;
 	while (fahr <= upper) {
 		celsius = (5.0/9.0) * (fahr - 32.0);
-		printf("%4.0f %6.1f\n", fahr, celsius);
+		printf("      %4.0f  %6.1f\n", fahr, celsius);
 		fahr = fahr + step;
 	}
 }
