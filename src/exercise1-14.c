5df14b922ac70087cc4f1400fc922e7ff92cdd1f
Joe Bonniwell <joe@joebonniwell.com>
2018-05-09 16:28:23 -0500

    Add exercise1-14

---
+++ src/exercise1-14.c
@@ -0,0 +1,10 @@
+/*
+ * Revise the main routine of the longest-line program so it will correctly print the length of arbitrarily long input lines, and as much as possible of the text.
+ */
+#include <stdio.h>
+
+int main()
+{
+	// TODO: Copy the longest line program
+	// Not quite sure about the directions on this one
+}
