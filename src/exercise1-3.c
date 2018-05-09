15f9e9d566ab1e2ccca5eecffd49198830b4f419
Joe Bonniwell <joe@joebonniwell.com>
2018-05-09 08:58:56 -0500

    Add exercise1-3

---
+++ src/exercise1-3.c
@@ -0,0 +1,21 @@
+/*
+ * Modify temperature conversion program to add table heading
+ */
+
+#include <stdio.h>
+
+int main() {
+	int lower, upper, step;
+	float fahr, celsius;
+
+	lower = 0;
+	upper = 300;
+	step = 20;
+
+	fahr = lower;
+	while (fahr <= upper) {
+		celsius = (5.0/9.0) * (fahr - 32.0);
+		printf("%4.0f %6.1f\n", fahr, celsius);
+		fahr = fahr + step;
+	}
+}
